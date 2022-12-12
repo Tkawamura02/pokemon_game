e07243412e8f3dc07d36506c564193e4ab483c29
Tiffany Kawamura <86638759+Tkawamura02@users.noreply.github.com>
2022-12-12 15:18:38 -0700

    Finished Code

---
+++ sublimecode/data/attacks.js
@@ -0,0 +1,16 @@
+const attacks = {
+	Tackle : {
+		name: 'Tackle',
+		damage: 10,
+		type: 'Normal',
+		color: 'black'
+	},
+	Fireball : {
+		name: 'Fireball',
+		damage: 25,
+		type: 'Fire',
+		color: 'red'
+	}
+}
+
+//to add attacks, make sure to populate in monsters.js and classes.js>animate information
